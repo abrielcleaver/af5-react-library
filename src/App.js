b5dd88839874a8acd1f1d0bced8befab21e41c11
Abriel Cleaver <abrielcleaver@gmail.com>
2022-01-07 03:22:24 -0700

    home page renders properly & booklist renders from book link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import BookList from './views/Books/BookList';
+import React from 'react';
+import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
+
 import './App.css';
+import BookList from './views/Books/BookList';
+import Home from './views/Home';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
+      <BrowserRouter>
+        <header>
+          <NavLink to="/" data-testid="main-link">
+            Home
+          </NavLink>
+          <NavLink to="/books" data-testid="books-link">
+            Books
+          </NavLink>
+        </header>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/books">
+            <BookList />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </main>
   );
 }
